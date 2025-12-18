bc3678cbd4dec53d043d3af3b6b2b4505a2d3ce6
dailyquote1705 <finaiagent.1705@gmail.com>
2025-12-18 11:46:16 -0800

    Create quotes.js

---
+++ quotes.js
@@ -0,0 +1,14 @@
+window.QUOTES = [
+  {
+    id: "jobs-love-work",
+    text: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs",
+    story: "Jobs emphasized that passion is the key driver behind meaningful work."
+  },
+  {
+    id: "angelou-strength",
+    text: "You may not control all the events that happen to you, but you can decide not to be reduced by them.",
+    author: "Maya Angelou",
+    story: "A reminder of resilience and choosing your attitude."
+  }
+];
